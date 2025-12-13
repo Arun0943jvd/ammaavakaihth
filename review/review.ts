f54b4e0556043da422f543774cee5873bab25561
Arunkumar velupula <srilatha.shirisha@gmail.com>
2025-12-13 18:39:01 -0500

    folders structure

--- review/review.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-review',
-  imports: [],
-  templateUrl: './review.html',
-  styleUrl: './review.scss',
-})
-export class ReviewComponent {}
